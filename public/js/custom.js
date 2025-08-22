bff2d70754bf4fe2c807869bfbbb67268cd29131
xlinkexe <114164438+xlinkexe@users.noreply.github.com>
2025-08-22 18:55:43 +0800

    Update custom.js

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,6 +1,7 @@
 let tianliGPT_postSelector = 'article';
 let tianliGPT_postURL = '*/technology/*';
-let tianliGPT_Title = '文章总结';
+let tianliGPT_BeginningText = 'ArtemisiaのBlog的这篇博文介绍了';
+let tianliGPT_theme = 'simple';
 let tianliGPT_key = 'S-J48A7SHZMBBEA0GV';
 if (typeof tianliGPT_key !== 'undefined' && tianliGPT_key) {
   postchat_checkSystemType();
