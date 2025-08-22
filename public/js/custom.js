379af42e0d899f1cf926d79b056745b3c4f764f9
xlinkexe <114164438+xlinkexe@users.noreply.github.com>
2025-08-22 18:20:27 +0800

    Update custom.js

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,5 +1,5 @@
 let tianliGPT_postSelector = 'article';
-let tianliGPT_postURL = '*/technology/*.html';
+let tianliGPT_postURL = '*/technology/*';
 let tianliGPT_Title = '文章总结';
 let tianliGPT_key = 'S-J48A7SHZMBBEA0GV';
 if (typeof tianliGPT_key !== 'undefined' && tianliGPT_key) {
