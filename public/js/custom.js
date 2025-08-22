0313eb3ed9daa6d87b3051043d94151033aca20d
xlinkexe <114164438+xlinkexe@users.noreply.github.com>
2025-08-22 20:27:57 +0800

    Update custom.js

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,6 +1,6 @@
 let tianliGPT_postSelector = 'article';
 let tianliGPT_postURL = '*/technology/*';
-let tianliGPT_Title = '精炼O.o?';
+let tianliGPT_Title = '精炼O.o?抓住文章，顷刻炼化ovo';
 let tianliGPT_BeginningText = 'ArtemisiaのBlog中的这篇博文介绍了';
 let tianliGPT_theme = 'simple';
 let tianliGPT_key = 'S-J48A7SHZMBBEA0GV';
