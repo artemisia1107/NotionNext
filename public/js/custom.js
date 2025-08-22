08ce38eac1ce95a2d4b188c2b3013935aefe4441
xlinkexe <114164438+xlinkexe@users.noreply.github.com>
2025-08-22 12:11:12 +0800

    Update custom.js

--- public/js/custom.js
+++ public/js/custom.js
@@ -2,3 +2,7 @@ let tianliGPT_postSelector = 'article';
 let tianliGPT_postURL = 'https://artemisia.icu/technology/*';
 let tianliGPT_Title = '文章总结';
 let tianliGPT_key = 'S-J48A7SHZMBBEA0GV';
+if (typeof tianliGPT_key !== 'undefined' && tianliGPT_key) {
+  postchat_checkSystemType();
+  window.tianliGPT.checkURLAndRun();
+}
