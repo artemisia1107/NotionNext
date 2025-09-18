693ac702b39c26c1417d0a478512f8a8cd7a034a
Artemisia <114164438+artemisia1107@users.noreply.github.com>
2025-09-18 17:54:48 +0800

    Update custom.js

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,5 +1,5 @@
 let tianliGPT_postSelector = 'article';
-let tianliGPT_postURL = '*/technology/*';
+let tianliGPT_postURL = 'https://artemisia.icu/technology/*';
 let tianliGPT_Title = '精炼O.o?抓住文章，顷刻炼化ovo';
 let tianliGPT_BeginningText = 'ArtemisiaのBlog中的这篇博文介绍了';
 let tianliGPT_key = 'S-J48A7SHZMBBEA0GV';
